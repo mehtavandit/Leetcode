3dbdef78b4f858e610618417fa88b5725806957e
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2024-05-17 17:19:04 -0400

    Create Prefix_to_Postfix.py

---
+++ Stack_Queues/Prefix_Infix_Postfix_Conversion_Problem/Prefix_to_Postfix.py
@@ -0,0 +1,43 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def preToPost(self, pre_exp):
+        # Code here
+        def isOperator(c):
+            if c == "*" or c == "+" or c == "-" or c == "/" or c == "^" or c == "(" or c == ")":
+                return True
+            else:
+                return False
+                
+                
+        stack = []
+        
+        i = len(pre_exp) - 1
+        
+        while i>=0:
+            
+            if not isOperator(pre_exp[i]):
+                stack.append(pre_exp[i])
+                i -= 1
+            else:
+                exp = stack.pop() + stack.pop() + pre_exp[i]  
+                stack.append(exp)
+                i -=1
+                
+        return stack.pop()
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        prefix = input()
+        ob = Solution()
+        res = ob.preToPost(prefix)
+        print(res)
+# } Driver Code Ends
