c139559ffa16bd7749c991728ef3673435ac27f7
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2024-05-30 09:33:31 -0400

    Create pq_using_binary_heap.py

---
+++ Heaps/pq_using_binary_heap.py
@@ -0,0 +1,81 @@
+H = [0] * 50
+size = -1
+
+def parent(i):
+
+    return (i-1) // 2
+
+def left_child(i):
+
+    return ((2*i) + 1)
+
+def right_child(i):
+
+    return ((2*i)+2)
+
+def shiftup(i):
+
+    while (i>0 and H[parent(i)] < H[i]):
+        swap(parent(i), i)
+        i = parent(i)
+
+def shiftdown(i):
+    max_index = i
+    l = left_child(i)
+    if (l<= size and H[l] > H[max_index]):
+        max_index = l
+
+    r = right_child(i)
+    if (r<=size and H[r] > H[max_index]):
+        max_index = r
+
+    if i!=max_index:
+        swap(i, max_index)
+        shiftdown(max_index)
+
+def insert(p):
+    global size
+    size = size + 1
+    H[size] = p
+    shiftup(size)
+
+def extract_max():
+    global size
+    result = H[0]
+    H[0] = H[size]
+    size -= 1
+    shiftdown(0)
+    return result
+
+def swap (i, j):
+    temp = H[i]
+    H[i] = H[j] 
+    H[j] = temp
+
+insert(4)
+insert(2)
+insert(8)
+insert(16)
+insert(24)
+insert(2)
+insert(6)
+insert(5)
+
+i=0
+print("Priority Queue : ", end = "") 
+while (i <= size) : 
+ 
+    print(H[i], end = " ") 
+    i += 1
+
+print(extract_max())
+
+i=0
+print("Priority Queue : ", end = "") 
+while (i <= size) : 
+ 
+    print(H[i], end = " ") 
+    i += 1
+
+
+
