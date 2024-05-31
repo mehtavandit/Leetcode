450d50873d6f0e59210da4414a8891dc31dc43b7
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2024-05-31 12:30:57 -0400

    Create kth_smalles_element_in_an_array.py

---
+++ Heaps/kth_smalles_element_in_an_array.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    def kthSmallest(self,arr, l, r, k):
+        '''
+        arr : given array
+        l : starting index of the array i.e 0
+        r : ending index of the array i.e size-1
+        k : find kth smallest element and return using this function
+        '''
+        import heapq
+        n = len(arr)
+        
+        pq = []
+        
+        for i in arr:
+            heapq.heappush(pq, i)
+            
+        f = k-1
+        
+        while f > 0:
+            heapq.heappop(pq)
+            f -= 1
+            
+        return pq[0]
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__': 
+    import random 
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=list(map(int,input().strip().split()))
+        k=int(input())
+        ob=Solution()
+        print(ob.kthSmallest(arr, 0, n-1, k))
+        
+# } Driver Code Ends
