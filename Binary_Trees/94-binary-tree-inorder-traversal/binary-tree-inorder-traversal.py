aa0987cd5f32553d9cd0f5b5c7b08b47d13f64cd
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2024-06-27 16:59:56 -0400

    Time: 34 ms (65.01%) | Memory: 16.5 MB (55.02%) - LeetSync

--- Binary_Trees/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ Binary_Trees/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -5,14 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-        self.helper(root, result)
-        return result
 
-    def helper(self, node: Optional[TreeNode], result: List[int]):
-        if node is None:
-            return
-        self.helper(node.left, result)
-        result.append(node.val)
-        self.helper(node.right, result)
+    # recursive
+    # def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     result = []
+    #     self.helper(root, result)
+    #     return result
+
+    # def helper(self, node: Optional[TreeNode], result: List[int]):
+    #     if node is None:
+    #         return
+    #     self.helper(node.left, result)
+    #     result.append(node.val)
+    #     self.helper(node.right, result)
+
+    # iterative
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        current = root
+        stack = []
+        inorder = []
+
+        while True:
+
+            if current is not None:
+                stack.append(current)
+                current = current.left
+            elif(stack):
+                current = stack.pop()
+                inorder.append(current.val)
+                current=current.right
+            else:
+                break
+
+        return inorder
