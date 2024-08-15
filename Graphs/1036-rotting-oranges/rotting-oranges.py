63bf8bbf8ae55de9a5b84543ee009723166f9084
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2024-08-15 18:09:17 -0400

    Time: 43 ms (91.78%) | Memory: 16.4 MB (83.95%) - LeetSync

---
+++ Graphs/1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        queue = deque()
+        fresh_oranges = 0
+        minutes = 0
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r,c))
+                elif grid[r][c] == 1:
+                    fresh_oranges += 1
+
+        if fresh_oranges == 0:
+            return 0
+
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+
+        while queue:
+            minutes += 1
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dx, dy in directions:
+                    nx = x + dx
+                    ny = y + dy
+
+                    if 0 <= nx < rows and 0<=ny<cols and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2
+                        fresh_oranges = fresh_oranges - 1
+                        queue.append((nx, ny))
+
+
+        if fresh_oranges == 0:
+            return minutes-1
+        else:
+            return -1
