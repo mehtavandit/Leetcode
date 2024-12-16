74cc01447992dd5a7e0fcd3dffb38324c6ffc303
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2024-12-16 12:39:30 -0500

    Time: 7 ms (57.46%) | Memory: 19.1 MB (6.06%) - LeetSync

--- Graphs/547-number-of-provinces/number-of-provinces.py
+++ Graphs/547-number-of-provinces/number-of-provinces.py
@@ -1,5 +1,6 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
         def dfs(city):
             for adjacent_city in range(n):
                 if isConnected[city][adjacent_city] == 1 and not visited[adjacent_city]:
@@ -9,13 +10,12 @@ class Solution:
         n = len(isConnected)
 
         visited = [False] * n
-        provinces = 0
+        province = 0
 
         for city in range(n):
-            if not visited[city] :
+            if not visited[city]:
                 visited[city] = True
-                provinces += 1
+                province += 1
                 dfs(city)
 
-
-        return provinces
+        return province
