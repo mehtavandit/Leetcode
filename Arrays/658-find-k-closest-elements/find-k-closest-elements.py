f2d3e072bced5e921b79aed24114c2d3f2d953cb
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2024-12-31 17:43:19 -0500

    Time: 7 ms (62.48%) | Memory: 19.1 MB (21.95%) - LeetSync

--- Arrays/658-find-k-closest-elements/find-k-closest-elements.py
+++ Arrays/658-find-k-closest-elements/find-k-closest-elements.py
@@ -2,25 +2,52 @@ from collections import defaultdict
 
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        dict_ = defaultdict(list)
+        # dict_ = defaultdict(list)
 
-        for num in arr:
-            dict_[abs(num-x)].append(num)
+        # for num in arr:
+        #     dict_[abs(num-x)].append(num)
 
-        sorted_dict = sorted(dict_.keys())
+        # sorted_dict = sorted(dict_.keys())
 
-        result = []
+        # result = []
 
-        for i in sorted_dict:
-            for num in dict_[i]:
-                result.append(num)
+        # for i in sorted_dict:
+        #     for num in dict_[i]:
+        #         result.append(num)
                 
-                if len(result) == k:
-                    break
+        #         if len(result) == k:
+        #             break
 
-            if len(result) == k:
-                break
+        #     if len(result) == k:
+        #         break
         
 
-        result.sort()
-        return result
+        # result.sort()
+        # return result
+
+        idx = 0
+        n = len(arr)
+
+        for i in range(n-1, 0, -1):
+            if (arr[i] <= x):
+                idx = i
+                break
+
+        left, right = idx, idx+1
+        result = []
+        for _ in range(k):
+            if left>=0 and right< len(arr):
+                if abs(arr[left] - x) <= abs(arr[right]-x):
+                    result.append(arr[left])
+                    left -= 1
+                else:
+                    result.append(arr[right])
+                    right += 1
+            elif left>=0:
+                result.append(arr[left])
+                left -= 1
+            else:
+                result.append(arr[right])
+                right+=1
+
+        return sorted(result)
