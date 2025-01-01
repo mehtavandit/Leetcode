81c0a0673de6b598ef5c8ae3aa0fea36179ece2d
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-01 12:22:32 -0500

    Time: 0 ms (100.00%) | Memory: 25.5 MB (23.19%) - LeetSync

--- Arrays/189-rotate-array/rotate-array.py
+++ Arrays/189-rotate-array/rotate-array.py
@@ -1,46 +1,16 @@
-import math
-
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
-    
         """
-
-
         n = len(nums)
-
-        if n<=1:
-            return
-
         k = k % n
-
-        # for i in range(0, k):
-
-        #     x = nums[n-1]
-
-        #     for j in range(n-1, 0, -1):
-
-        #         nums[j] = nums[j-1]
-
-        #     nums[0] = x
-
-        for i in range (0, floor(n/2)):
-
-            x = nums[i]
-            nums[i] = nums[n-i-1]
-            nums[n-i-1] = x
-
-        for i in range (0,floor(k/2)):
-            x = nums[i]
-            nums[i] = nums[k-i-1]
-            nums[k-i-1] = x
-
-        for i in range(0, floor((n-k)/2)):
-            x = nums[i+k]
-            nums[i+k] = nums[n-i-1]
-            nums[n-i-1] = x 
-
         
-
+        # Reverse the entire array
+        nums.reverse()
         
+        # Reverse the first k elements
+        nums[:k] = reversed(nums[:k])
+        
+        # Reverse the remaining n-k elements
+        nums[k:] = reversed(nums[k:])
