0c82a2b8e1b38a1d9de4946a9256ea231c629bb5
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-06 12:11:41 -0500

    Time: 3 ms (50.49%) | Memory: 23.3 MB (15.73%) - LeetSync

---
+++ Linked_List/143-reorder-list/reorder-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+
+        nodes = []
+        curr = head
+        while(curr):
+            nodes.append(curr)
+            curr = curr.next
+
+
+        left = 0 #left right are just the indexes, you need nodes[index]
+        right = len(nodes) - 1 
+
+        while left < right:
+            nodes[left].next = nodes[right]
+            left += 1
+            if left >= right:
+                break
+            nodes[right].next = nodes[left]
+            right -= 1
+
+        nodes[left].next = None
