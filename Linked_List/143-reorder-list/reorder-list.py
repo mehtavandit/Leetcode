7772023dcd2b9b79ff1d30cf39e87cd0d64b8b14
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-06 12:43:22 -0500

    Time: 7 ms (23.72%) | Memory: 23.3 MB (15.73%) - LeetSync

--- Linked_List/143-reorder-list/reorder-list.py
+++ Linked_List/143-reorder-list/reorder-list.py
@@ -9,23 +9,49 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
 
+        slow, fast = head, head.next
 
-        nodes = []
-        curr = head
-        while(curr):
-            nodes.append(curr)
-            curr = curr.next
+        while fast and fast.next: #finding the middle of a list
+            slow = slow.next
+            fast = fast.next.next
 
 
-        left = 0 #left right are just the indexes, you need nodes[index]
-        right = len(nodes) - 1 
+        second = slow.next #breaking the list into two part first half and second half
+        slow.next = None
+        prev = None
 
-        while left < right:
-            nodes[left].next = nodes[right]
-            left += 1
-            if left == right:
-                break
-            nodes[right].next = nodes[left]
-            right -= 1
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
 
-        nodes[left].next = None
+        first, second = head, prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+        
+
+
+        # nodes = []
+        # curr = head
+        # while(curr):
+        #     nodes.append(curr)
+        #     curr = curr.next
+
+
+        # left = 0 #left right are just the indexes, you need nodes[index]
+        # right = len(nodes) - 1 
+
+        # while left < right:
+        #     nodes[left].next = nodes[right]
+        #     left += 1
+        #     if left == right:
+        #         break
+        #     nodes[right].next = nodes[left]
+        #     right -= 1
+
+        # nodes[left].next = None
