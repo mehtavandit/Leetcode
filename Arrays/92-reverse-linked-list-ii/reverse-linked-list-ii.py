5882e9b68e1eb9bfd500520556dd1d921c04336a
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-08 11:58:36 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (5.37%) - LeetSync

--- Arrays/92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ Arrays/92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -5,27 +5,53 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # if not head or left == right:
+        #     return head
+
+        # nodes = []
+
+        # current = head
+
+        # while current:
+        #     nodes.append(current)
+        #     current = current.next
+
+        # left_index = left - 1
+        # right_index = right - 1
+
+        # while left_index < right_index:
+        #     nodes[left_index], nodes[right_index] = nodes[right_index], nodes[left_index]
+        #     left_index += 1
+        #     right_index -= 1
+
+        # for i in range(len(nodes) - 1):
+        #     nodes[i].next = nodes[i+1]
+        # nodes[-1].next = None
+
+        # return nodes[0]
+
+
         if not head or left == right:
             return head
 
-        nodes = []
+        dummy = ListNode(0)
+        dummy.next = head
+        pre = dummy
 
-        current = head
+        for _ in range(left-1):
+            pre = pre.next
 
-        while current:
-            nodes.append(current)
-            current = current.next
+        curr = pre.next
+        prev = None
 
-        left_index = left - 1
-        right_index = right - 1
+        for _ in range (right - left + 1):
+            nn = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nn
 
-        while left_index < right_index:
-            nodes[left_index], nodes[right_index] = nodes[right_index], nodes[left_index]
-            left_index += 1
-            right_index -= 1
+        pre.next.next = curr
+        pre.next = prev
 
-        for i in range(len(nodes) - 1):
-            nodes[i].next = nodes[i+1]
-        nodes[-1].next = None
 
-        return nodes[0]
+        return dummy.next
