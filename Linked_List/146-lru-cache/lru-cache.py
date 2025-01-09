f19b1469d68c7defec7015f195ed505dba4309f1
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-09 11:42:56 -0500

    Time: 136 ms (61.71%) | Memory: 78.6 MB (15.14%) - LeetSync

---
+++ Linked_List/146-lru-cache/lru-cache.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.address = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node: Node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def add(self, node:Node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.address:
+            node = self.address[key]
+            self.remove(node)
+            self.add(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.address:
+            node = self.address[key]
+            node.value = value
+            self.remove(node)
+            self.add(node)
+        else:
+            if len(self.address) >= self.capacity:
+                node = self.tail.prev
+                self.remove(node)
+                del self.address[node.key]
+            nn = Node(key, value)
+            self.address[key] = nn
+            self.add(nn)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
