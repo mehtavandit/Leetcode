01e5b0faa1b335dee65d30a75010d343abca6e94
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-09 15:56:46 -0500

    Time: 7 ms (91.10%) | Memory: 20 MB (43.03%) - LeetSync

--- Linked_List/23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ Linked_List/23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,21 +5,62 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        result = []
+        # result = []
 
-        for temp in lists:
-            while temp:
-                result.append(temp.val)
-                temp = temp.next
+        # for temp in lists:
+        #     while temp:
+        #         result.append(temp.val)
+        #         temp = temp.next
 
-        result.sort()
+        # result.sort()
 
-        curr = ListNode(0)
-        temp = curr
+        # curr = ListNode(0)
+        # temp = curr
 
-        for node in result:
-            nn = ListNode(node)
-            temp.next = nn
-            temp = nn
+        # for node in result:
+        #     nn = ListNode(node)
+        #     temp.next = nn
+        #     temp = nn
 
-        return curr.next
+        # return curr.next
+
+        if not lists or len(lists) == 0:
+            return None
+
+        
+
+        while len(lists) > 1:
+            merged_list = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                merged_list.append(self.mergetwolists(l1, l2))
+            lists = merged_list
+
+        return lists[0]
+
+    def mergetwolists(self, l1, l2):
+        dummy = ListNode()
+        temp = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+
+        while l1:
+            temp.next = l1
+            l1 = l1.next
+            temp = temp.next
+
+        while l2:
+            temp.next = l2
+            l2 = l2.next
+            temp = temp.next
+
+        return dummy.next
+
