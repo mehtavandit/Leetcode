e374d7d1e10a1b45421f329b0bdad54f616bb6f7
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-11 11:04:11 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (23.04%) - LeetSync

--- Arrays/75-sort-colors/sort-colors.py
+++ Arrays/75-sort-colors/sort-colors.py
@@ -3,14 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        counter = 1
+        left, right = 0, len(nums) - 1
+        i = 0
 
-        while counter <= n:
-            for i in range(0, n-1):
-                if nums[i] > nums[i+1]:
-                    x = nums[i+1]
-                    nums[i+1] = nums[i]
-                    nums[i] = x
-
-            counter += 1
+        while (i<=right):
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+            else:
+                i += 1
