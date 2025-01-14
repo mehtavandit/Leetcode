68fc74bbf85c529e22c08cf35a3907d085e2a6d2
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-13 20:11:32 -0500

    Time: 15 ms (14.45%) | Memory: 21.7 MB (12.04%) - LeetSync

--- Stack_Queues/155-min-stack/min-stack.py
+++ Stack_Queues/155-min-stack/min-stack.py
@@ -9,27 +9,26 @@ class MinStack:
     def push(self, val: int) -> None:
         self.main_stack.put(val)
 
-        if(self.min_stack.qsize() == 0):
+        if self.min_stack.qsize() == 0:
             self.min_stack.put(val)
         else:
             top_value = self.min_stack.queue[-1]
+
             if top_value < val:
                 self.min_stack.put(top_value)
             else:
                 self.min_stack.put(val)
+
 
     def pop(self) -> None:
         val1 = self.main_stack.get()
         val2 = self.min_stack.get()
 
     def top(self) -> int:
-         return self.main_stack.queue[-1]
+        return self.main_stack.queue[-1]
 
     def getMin(self) -> int:
-        # val1 = self.main_stack.get()
-        # val2 = self.min_stack.get()
         return self.min_stack.queue[-1]
-
 
 
 # Your MinStack object will be instantiated and called as such:
