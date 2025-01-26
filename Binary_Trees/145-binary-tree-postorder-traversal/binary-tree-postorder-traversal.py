6a951ffbe6c82b9ede1e1069c0ba790944fc9f75
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-26 11:34:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (38.58%) - LeetSync

--- Binary_Trees/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ Binary_Trees/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -5,44 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-    #recursive
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
-        self.helper(root, result)
+        self.helper(root,result)
         return result
 
-    def helper(self, node: Optional[TreeNode], result: List[int]):
+    def helper(self, node:Optional[TreeNode], result:List[int]):
         if node is None:
             return
+
         self.helper(node.left, result)
+
         self.helper(node.right, result)
+
         result.append(node.val)
 
-    # #using two stacks
-    # def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-    #     stack1 = []
-    #     stack2 = []
-
-    #     postorder = []
-
-    #     if root is None:
-    #         return postorder
-
-    #     stack1.append(root)
-
-    #     while stack1:
-    #         current = stack1.pop()
-
-    #         stack2.append(current)
-
-    #         if current.left is not None:
-    #             stack1.append(current.left)
-            
-    #         if current.right is not None:
-    #             stack1.append(current.right)
-
-    #     while stack2:
-    #         postorder.append(stack2.pop)
-
-    #     return postorder
+        
