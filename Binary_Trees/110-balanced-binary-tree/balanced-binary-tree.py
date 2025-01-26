78795bbad7a3b6a97b0259d223d8fa6601fdf0e1
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-26 12:57:03 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (43.37%) - LeetSync

--- Binary_Trees/110-balanced-binary-tree/balanced-binary-tree.py
+++ Binary_Trees/110-balanced-binary-tree/balanced-binary-tree.py
@@ -4,26 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from queue import Queue
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.check_balance(root) != -1
+        return self.helper(root) != -1
 
-    def check_balance(self, node: Optional[TreeNode]) -> int:
+    def helper(self, node: TreeNode):
         if not node:
             return 0
         
-        left_height = self.check_balance(node.left)
-        if left_height == -1:
+        lheight = self.helper(node.left)
+
+        if lheight == -1:
+            return -1
+
+        rheight = self.helper(node.right)
+
+        if rheight == -1:
+            return -1
+
+        if abs(lheight - rheight) > 1:
             return -1
         
-        right_height = self.check_balance(node.right)
-        if right_height == -1:
-            return -1
-        
-        if abs(left_height - right_height) > 1:
-            return -1
-        
-        return max(left_height, right_height) + 1
+        return max(lheight, rheight) + 1
