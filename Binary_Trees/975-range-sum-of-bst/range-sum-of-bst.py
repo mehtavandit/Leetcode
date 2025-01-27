11dc25292d548e287f3172a8eaf871fa48e2a1a9
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-27 15:59:30 -0500

    Time: 0 ms (100.00%) | Memory: 25.2 MB (41.45%) - LeetSync

--- Binary_Trees/975-range-sum-of-bst/range-sum-of-bst.py
+++ Binary_Trees/975-range-sum-of-bst/range-sum-of-bst.py
@@ -5,21 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    #     return self.helper(root, low, high)
+
+    # def helper(self, node:TreeNode, low, high):
+    #     if node is None:
+    #         return 0
+
+    #     x = node.val
+    #     current_sum = 0
+
+    #     if low <= x and x <= high:
+    #         current_sum += x
+
+    #     current_sum += self.helper(node.left, low, high)
+    #     current_sum += self.helper(node.right, low, high)
+
+    #     return current_sum
+
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        return self.helper(root, low, high)
 
-    def helper(self, node:TreeNode, low, high):
-        if node is None:
-            return 0
+        def dfs(node):
+            if not node:
+                return
+            if low<=node.val<=high:
+                self.total += node.val
+                dfs(node.left)
+                dfs(node.right)
+            elif node.val < low:
+                dfs(node.right)
+            elif node.val > high:
+                dfs(node.left)
 
-        x = node.val
-        current_sum = 0
-
-        if low <= x and x <= high:
-            current_sum += x
-
-        current_sum += self.helper(node.left, low, high)
-        current_sum += self.helper(node.right, low, high)
-
-        return current_sum
+        self.total = 0
+        dfs(root)
+        return self.total
 
