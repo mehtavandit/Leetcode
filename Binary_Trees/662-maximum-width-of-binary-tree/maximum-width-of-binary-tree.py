c5b9cf8215f6e862de58ca7effa5b88650b384ce
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-01-31 15:28:24 -0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (45.87%) - LeetSync

--- Binary_Trees/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ Binary_Trees/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -5,37 +5,31 @@
 #         self.left = left
 #         self.right = right
 from collections import deque
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if root is None:
+        if not root:
             return 0
         
-        q = deque([(root, 0)])
         max_width = 0
+        q = deque([(root, 0)])
 
         while q:
             size = len(q)
-            min_index = q[0][1]
-            first_index, last_index = q[0][1], q[-1][1]
+            _, firstindex = q[0]
+            _, lastindex = q[-1]
 
-            for _ in range(size):
+            max_width = max(max_width, lastindex - firstindex + 1)
+
+            for i in range(size):
                 node, index = q.popleft()
 
                 if node.left:
-                    q.append((node.left, 2*index + 1))
+                    q.append((node.left, 2 * index ))
                 if node.right:
-                    q.append((node.right, 2*index + 2))
-            
-            max_width = max(max_width, last_index - first_index + 1)
-        
+                    q.append((node.right, 2 * index + 1))
+
+
         return max_width
 
-
+                
