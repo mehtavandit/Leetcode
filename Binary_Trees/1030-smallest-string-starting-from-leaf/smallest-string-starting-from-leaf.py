358c4ee9e10ae563a32a022245832dff2863164b
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-02-09 10:48:47 -0500

    Time: 3 ms (75.47%) | Memory: 19.3 MB (28.95%) - LeetSync

--- Binary_Trees/1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
+++ Binary_Trees/1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.py
@@ -6,26 +6,47 @@
 #         self.right = right
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-        self.base = 97
-        self.smallest_string = None
+        # self.base = 97
+        # self.smallest_string = None
+
+        # def dfs(root, path):
+        #     if root is None:
+        #         return
+
+        #     path += chr(self.base + root.val)
+
+        #     if root.left is None and root.right is None:
+        #         reversed_path = "".join(reversed(path))
+        #         if self.smallest_string is None:
+        #             self.smallest_string = reversed_path
+        #         else:
+        #             self.smallest_string = min(self.smallest_string, reversed_path)
+                
+
+        #     dfs(root.left, path)
+        #     dfs(root.right, path)
+
+            
+        # dfs(root, "")
+        # return self.smallest_string
+
+        paths = []
 
         def dfs(root, path):
             if root is None:
                 return
 
-            path += chr(self.base + root.val)
+            path.append(chr(root.val + 97))
 
             if root.left is None and root.right is None:
-                reversed_path = "".join(reversed(path))
-                if self.smallest_string is None:
-                    self.smallest_string = reversed_path
-                else:
-                    self.smallest_string = min(self.smallest_string, reversed_path)
+                value = "".join(path)
+                paths.append(value[::-1])
                 
 
             dfs(root.left, path)
             dfs(root.right, path)
+            path.pop()
 
-            
-        dfs(root, "")
-        return self.smallest_string
+        dfs(root, [])
+        paths.sort()
+        return paths[0]
