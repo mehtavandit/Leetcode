e8dea32454b3c175ba866fcfc93fd6b4b88852bf
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-02-20 09:36:58 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.57%) - LeetSync

--- Amazon/2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ Amazon/2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -1,3 +1,11 @@
+import heapq
+
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        return len(set(nums) - {0})  
+        # return len(set(nums) - {0})  
+
+        min_heap = list(set(nums) - {0})
+
+        heapq.heapify(min_heap)
+
+        return len(min_heap)
