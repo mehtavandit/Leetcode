8b53ecd17c295ca252dd3fa1a7e7c2d3e3e0d51f
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-02-28 17:04:30 -0500

    Time: 7 ms (32.87%) | Memory: 19.9 MB (75.75%) - LeetSync

---
+++ Amazon/253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -0,0 +1,19 @@
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        if not intervals:
+            return 0
+
+        min_heap = []
+        intervals.sort()
+
+        for start, end in intervals:
+            if min_heap and min_heap[0] <= start:
+                heapq.heappop(min_heap)
+            
+            heapq.heappush(min_heap, end)
+
+        return len(min_heap)
+
+        
