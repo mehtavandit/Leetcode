ca07c691e4896f3a2d3d21708d8779079a4d3a85
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-03 11:59:49 -0500

    Time: 33 ms (12.85%) | Memory: 35.2 MB (10.10%) - LeetSync

---
+++ Amazon/217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counter = Counter(nums)
+
+        more_than_one = [nums for nums, freq in counter.items() if freq > 1]
+
+        if len(more_than_one) == 0:
+            return False
+        
+        return True
