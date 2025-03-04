6fabe7e3f14e79b01bd9773a25963dd4ceae03d7
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-04 10:00:52 -0500

    Time: 47 ms (52.31%) | Memory: 19 MB (18.54%) - LeetSync

--- Amazon/117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ Amazon/117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -12,26 +12,55 @@ class Node:
 from collections import deque
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if root is None:
-            return
+        # if root is None:
+        #     return
 
-        q = deque([root])
+        # q = deque([root])
 
-        while q:
-            size = len(q)
-            prev = None
+        # while q:
+        #     size = len(q)
+        #     prev = None
 
-            for _ in range(size):
-                node = q.popleft()
-                if prev:
-                    prev.next = node
-                prev = node
+        #     for _ in range(size):
+        #         node = q.popleft()
+        #         if prev:
+        #             prev.next = node
+        #         prev = node
 
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
 
-            prev.next = None
+        #     prev.next = None
+
+        # return root
+
+        #TC :- O(n) as it visits each node once and Space complexity O(n) due to queue storage
+
+        if not root:
+            return None
+
+        current = root
+
+        while current:
+            dummy = Node(0)
+            prev = dummy
+            temp = current
+
+            while temp:
+                if temp.left:
+                    prev.next = temp.left
+                    prev = prev.next
+                 
+                if temp.right:
+                    prev.next = temp.right
+                    prev = prev.next
+
+                temp = temp.next
+
+            current = dummy.next
+            dummy.next = None
+
 
         return root
