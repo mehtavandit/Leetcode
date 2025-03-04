4816bf1535bb76f018bd95875df781e359c5df9d
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-04 09:41:31 -0500

    Time: 56 ms (28.22%) | Memory: 18.9 MB (86.85%) - LeetSync

--- Amazon/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ Amazon/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -12,27 +12,51 @@ class Node:
 from collections import deque
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        # if not root:
+        #     return None
+
+        # q = deque([root])
+
+        # while q:
+        #     size = len(q)
+        #     prev = None
+
+        #     for _ in range(size):
+        #         node = q.popleft()
+        #         if prev:
+        #             prev.next = node
+        #         prev = node
+
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+
+        #     prev.next = None
+
+        
+        # return root
+
+        #TC :- O(n) as it visits each node once and Space complexity O(n) due to queue storage
+
         if not root:
             return None
 
-        q = deque([root])
+        leftmost = root
 
-        while q:
-            size = len(q)
-            prev = None
+        while leftmost.left:
+            current = leftmost
 
-            for _ in range(size):
-                node = q.popleft()
-                if prev:
-                    prev.next = node
-                prev = node
+            while current:
+                current.left.next = current.right
 
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+                if current.next:
+                    current.right.next = current.next.left
 
-            prev.next = None
+                current = current.next
 
-        
+            
+            leftmost = leftmost.left
+
         return root
+
