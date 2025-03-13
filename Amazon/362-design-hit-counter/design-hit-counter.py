b01c298fd05d79699c520a14116f2930adf5559e
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-13 18:22:38 -0400

    Time: 3 ms (9.58%) | Memory: 18 MB (39.40%) - LeetSync

--- Amazon/362-design-hit-counter/design-hit-counter.py
+++ Amazon/362-design-hit-counter/design-hit-counter.py
@@ -2,17 +2,44 @@ from collections import deque
 
 class HitCounter:
 
+    # def __init__(self):
+    #     self.hits = deque()
+
+    # def hit(self, timestamp: int) -> None: #O(1)
+    #     self.hits.append(timestamp)
+
+    # def getHits(self, timestamp: int) -> int: #O(n)
+    #     while self.hits and self.hits[0] <= timestamp - 300:
+    #         self.hits.popleft()
+
+    #     return len(self.hits)
+
+    #the above approach is not scalable when there are million hits per second bcoz sorting them is a time consuming process
+
+    #To solve this use a circular array of size 300 (fixed size bucket)
+
     def __init__(self):
-        self.hits = deque()
+        self.size = 300
+        self.time = [0] * self.size
+        self.count = [0] * self.size
 
-    def hit(self, timestamp: int) -> None:
-        self.hits.append(timestamp)
 
-    def getHits(self, timestamp: int) -> int:
-        while self.hits and self.hits[0] <= timestamp - 300:
-            self.hits.popleft()
+    def hit(self, timestamp: int) -> None: #O(1)
+        index = timestamp % self.size
 
-        return len(self.hits)
+        if self.time[index] != timestamp:
+            self.time[index] = timestamp
+            self.count[index] = 1
+        else:
+            self.count[index] += 1
+
+    def getHits(self, timestamp: int) -> int: #O(n)
+        total_hits = 0
+        for i in range(self.size):
+            if timestamp - self.time[i] < self.size:
+                total_hits += self.count[i]
+
+        return total_hits
 
 
 # Your HitCounter object will be instantiated and called as such:
