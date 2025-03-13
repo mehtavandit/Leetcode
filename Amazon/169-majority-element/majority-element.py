b5f5e945d985cb85740fb4aff7be19cbc962a015
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-13 13:34:02 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.54%) - LeetSync

--- Amazon/169-majority-element/majority-element.py
+++ Amazon/169-majority-element/majority-element.py
@@ -2,8 +2,11 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
+        # count = Counter(nums)
 
-        for num, freq in count.items():
-            if freq > len(nums) // 2:
-                return num
+        # for num, freq in count.items():
+        #     if freq > len(nums) // 2:
+        #         return num
+
+        nums.sort()  # Sort the array
+        return nums[len(nums) // 2]
