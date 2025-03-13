7aad863883716444d053b10daff3c1bec9bee973
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-13 18:05:08 -0400

    Time: 69 ms (93.99%) | Memory: 20.2 MB (72.37%) - LeetSync

--- Amazon/348-design-tic-tac-toe/design-tic-tac-toe.py
+++ Amazon/348-design-tic-tac-toe/design-tic-tac-toe.py
@@ -1,40 +1,67 @@
 class TicTacToe:
 
+    # def __init__(self, n: int):
+    #     self.n = n
+    #     self.board = [[0]*n for _ in range(n) ]
+
+    # def move(self, row: int, col: int, player: int) -> int:
+    #     self.board[row][col] = player
+
+    #     if self.check_row(row, player) or self.check_col(col, player) or (row == col and self.check_diagonal(player)) or (row == self.n - col - 1 and self.check_antidiagonal(player)):
+    #         return player
+        
+    #     return 0
+
+    # def check_row(self, row, player):
+    #     for col in range(self.n):
+    #         if self.board[row][col] != player:
+    #             return False
+    #     return True
+
+    # def check_col(self, col, player):
+    #     for row in range(self.n):
+    #         if self.board[row][col] != player:
+    #             return False
+    #     return True
+
+    # def check_diagonal(self, player):
+    #     for i in range(self.n):
+    #         if self.board[i][i] != player:
+    #             return False
+    #     return True
+
+    # def check_antidiagonal(self, player):
+    #     for i in range(self.n):
+    #         if self.board[i][self.n-i-1] != player:
+    #             return False
+    #     return True
+
+    #For every move we check each sell 4 times i.e O(4 * n) = O(n)
+    #TC :- O(n2) for the board
+
     def __init__(self, n: int):
         self.n = n
-        self.board = [[0]*n for _ in range(n) ]
+        self.rows = [0] * n
+        self.cols = [0] * n
+        self.diagonal = 0
+        self.anti_diagonal = 0
 
     def move(self, row: int, col: int, player: int) -> int:
-        self.board[row][col] = player
+        current_player = 1 if player == 1 else -1
 
-        if self.check_row(row, player) or self.check_col(col, player) or (row == col and self.check_diagonal(player)) or (row == self.n - col - 1 and self.check_antidiagonal(player)):
+        self.rows[row] += current_player
+        self.cols[col] += current_player
+
+        if row == col:
+            self.diagonal += current_player
+
+        if col == self.n - row - 1:
+            self.anti_diagonal += current_player
+
+        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diagonal) == self.n or abs(self.anti_diagonal) == self.n:
             return player
-        
+
         return 0
-
-    def check_row(self, row, player):
-        for col in range(self.n):
-            if self.board[row][col] != player:
-                return False
-        return True
-
-    def check_col(self, col, player):
-        for row in range(self.n):
-            if self.board[row][col] != player:
-                return False
-        return True
-
-    def check_diagonal(self, player):
-        for i in range(self.n):
-            if self.board[i][i] != player:
-                return False
-        return True
-
-    def check_antidiagonal(self, player):
-        for i in range(self.n):
-            if self.board[i][self.n-i-1] != player:
-                return False
-        return True
 
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
