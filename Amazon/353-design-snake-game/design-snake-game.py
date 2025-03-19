81b77ef4f2387f129dedb9be1b4c631f5301197b
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-18 20:32:46 -0400

    Time: 186 ms (70.44%) | Memory: 19.4 MB (68.57%) - LeetSync

---
+++ Amazon/353-design-snake-game/design-snake-game.py
@@ -0,0 +1,46 @@
+class SnakeGame:
+
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        self.snake = deque()
+        self.snake.append((0,0))
+        self.width = width
+        self.height = height
+        self.food = food
+        self.food_index = 0
+        self.movement = {
+            'U': [-1, 0],
+            'D' : [1,0],
+            'L' : [0, -1],
+            'R' : [0, 1]
+        }
+        self.snake_set = {(0,0) : 1}
+
+    def move(self, direction: str) -> int:
+        newHead = (self.snake[0][0] + self.movement[direction][0], self.snake[0][1] + self.movement[direction][1])
+
+        if newHead[0] < 0 or newHead[0] >= self.height: #check if it goes out of height
+            return -1
+
+        if newHead[1] < 0 or newHead[1] >= self.width: #check if it goes out of widht
+            return -1
+
+        if newHead in self.snake and newHead != self.snake[-1]: #if it bites itself
+            return -1
+
+        new_food_item = self.food[self.food_index] if self.food_index < len(self.food) else None
+
+        if new_food_item and newHead[0] == new_food_item[0] and newHead[1] == new_food_item[1]:
+            self.food_index += 1
+        else:
+            tail = self.snake.pop()
+            del self.snake_set[tail]
+
+        self.snake.appendleft(newHead)
+        self.snake_set[newHead] = 1
+
+        return len(self.snake) - 1
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
