debf14698ca833d89581a9e579105700971bb5cb
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-21 18:46:36 -0400

    Time: 283 ms (10.36%) | Memory: 37.6 MB (8.43%) - LeetSync

--- Amazon/3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ Amazon/3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -4,14 +4,14 @@ class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         min_heap = []
 
-        for i in range(len(nums)):
+        for i in range(len(nums)): #inserting n elements into heap O(nlogn)
             heapq.heappush(min_heap, nums[i])
 
         counter = 0
 
-        while min_heap[0] < k:
+        while min_heap[0] < k and len(min_heap) >= 2:
             counter += 1
-            x = heapq.heappop(min_heap)
+            x = heapq.heappop(min_heap) #to take out element it is O(logn) and at worst you have to take out n elemets so O(nlogn)
             y = heapq.heappop(min_heap)
 
             new_num = (min(x,y) * 2 + max(x,y))
