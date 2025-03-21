2934f3aaca472e08a766bca9258eede0d6399ca0
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-21 13:44:23 -0400

    Time: 7 ms (67.75%) | Memory: 17.9 MB (41.36%) - LeetSync

--- Amazon/2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ Amazon/2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,21 +1,46 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        # n = len(A)
+
+        # prefix_arr = [0] * n
+
+        # elements_A, elements_B = set(), set()
+
+        # for i in range(n):
+        #     elements_A.add(A[i])
+        #     elements_B.add(B[i])
+
+        #     count = 0
+
+        #     for element in elements_A:
+        #         if element in elements_B:
+        #             count += 1
+
+        #     prefix_arr[i] = count
+
+        # return prefix_arr
+
+        #TC = O(n2) SC = O(n)
+
+        # For O(n) see editorial
+
+        result = []
         n = len(A)
 
-        prefix_arr = [0] * n
-
-        elements_A, elements_B = set(), set()
+        freq = [0] * (n+1)
+        count = 0
 
         for i in range(n):
-            elements_A.add(A[i])
-            elements_B.add(B[i])
+            freq[A[i]] += 1
 
-            count = 0
+            if freq[A[i]] == 2:
+                count += 1
 
-            for element in elements_A:
-                if element in elements_B:
-                    count += 1
+            freq[B[i]] += 1
 
-            prefix_arr[i] = count
+            if freq[B[i]] == 2:
+                count += 1
 
-        return prefix_arr
+            result.append(count)
+
+        return result
