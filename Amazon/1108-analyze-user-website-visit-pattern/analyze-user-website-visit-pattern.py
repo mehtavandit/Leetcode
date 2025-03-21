4b7cec4b34476ed3d1346f4c081e25a63be35be5
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-21 18:21:32 -0400

    Time: 11 ms (46.89%) | Memory: 18.5 MB (31.25%) - LeetSync

--- Amazon/1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ Amazon/1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -7,7 +7,7 @@ class Solution:
         # Step 1: Group the websites visited by each user in timestamp order
         user_visited = defaultdict(list)
 
-        for user, _, site in sorted(zip(username, timestamp, website), key=lambda x: x[1]):
+        for user, _, site in sorted(zip(username, timestamp, website)):
             user_visited[user].append(site)
 
         print(user_visited)
@@ -19,6 +19,8 @@ class Solution:
             unique_triples = set(itertools.combinations(visited_websites, 3))
             for triple in unique_triples:
                 counter[triple] += 1
+
+        print(counter)
 
         # Step 3: Find the most visited pattern with tie-breaking
         pattern, count = None, 0
