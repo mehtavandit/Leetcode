46267c7153e581d1b08a40a09dfab0691947ea45
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-03-25 15:37:04 -0400

    Time: 147 ms (36.92%) | Memory: 19.2 MB (27.24%) - LeetSync

---
+++ Amazon/621-task-scheduler/task-scheduler.py
@@ -0,0 +1,24 @@
+import heapq
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+
+        heap = [-cnt for cnt in count.values()]
+
+        heapq.heapify(heap)
+
+        time = 0 
+        q = deque()
+
+        while heap or q:
+            time += 1
+
+            if heap:
+                cnt = 1+heapq.heappop(heap)
+                if cnt!=0:
+                    q.append((cnt, time+n))
+
+            if q and q[0][1] == time:
+                heapq.heappush(heap, q.popleft()[0])
+
+        return time
