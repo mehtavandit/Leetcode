ee6713c5b2d0d2727eecc8a382395f887ddaccb8
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-01 15:32:01 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.64%) - LeetSync

--- DP/70-climbing-stairs/climbing-stairs.py
+++ DP/70-climbing-stairs/climbing-stairs.py
@@ -12,18 +12,33 @@ class Solution:
 
         #Memoization
 
+        # dp = [-1] * (n+1)
+
+        # def nested_fun(n):
+        #     if n <= 1:
+        #         return 1
+
+        #     if dp[n] != -1:
+        #         return dp[n]
+            
+        #     dp[n] = nested_fun(n-1) + nested_fun(n-2)
+
+        #     return dp[n]
+
+        # result = nested_fun(n)
+        # return result
+
+        #Tabulation
+
+        if n <= 1:
+            return 1
+
         dp = [-1] * (n+1)
 
-        def nested_fun(n):
-            if n <= 1:
-                return 1
+        dp[0] = 1
+        dp[1] = 1
 
-            if dp[n] != -1:
-                return dp[n]
-            
-            dp[n] = nested_fun(n-1) + nested_fun(n-2)
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
 
-            return dp[n]
-
-        result = nested_fun(n)
-        return result
+        return dp[n]
