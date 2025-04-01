7db545781ba0336fc21156ecb67db5ff9063c270
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-01 14:51:50 -0400

    Time: 32 ms (91.66%) | Memory: 17.8 MB (41.48%) - LeetSync

--- DP/1013-fibonacci-number/fibonacci-number.py
+++ DP/1013-fibonacci-number/fibonacci-number.py
@@ -1,5 +1,33 @@
 class Solution:
     def fib(self, n: int) -> int:
+
+        #Memoization Approach
+        # dp = [-1] * (n+1)
+
+        # def nested_function(n):
+        #     if n<= 1:
+        #         return n
+
+        #     if dp[n] != -1:
+        #         return dp[n]
+
+        #     dp[n] = nested_function(n-1) + nested_function(n-2)
+        #     return dp[n]
+
+        # result = nested_function(n)
+        # return result
+
+        #tabulation (bottom - up)
+
         if n<=1:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+
+        dp = [-1] * (n+1)
+
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
