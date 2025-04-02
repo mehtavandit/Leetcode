d7627d08a13a3ec0bc1720e0eca2d38b5f659ccc
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-02 19:44:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.85%) - LeetSync

--- DP/62-unique-paths/unique-paths.py
+++ DP/62-unique-paths/unique-paths.py
@@ -4,22 +4,38 @@ class Solution:
         rows = m
         cols = n
 
+        #Memoization
+        # dp = [ [-1 for _ in range(cols)] for _ in range(rows)]
+
+        # print(dp)
+
+
+        # def rec(row, col):
+        #     if row == 0 or col == 0:
+        #         return 1
+
+        #     if dp[row][col] != -1:
+        #         return dp[row][col]
+
+        #     up = rec(row-1, col)
+        #     left = rec(row, col - 1)
+
+        #     dp[row][col] =  up + left
+        #     return dp[row][col]
+
+        # return rec(rows-1, cols-1)
+
+        #Tabulation
+
         dp = [ [-1 for _ in range(cols)] for _ in range(rows)]
 
-        print(dp)
+        for i in range(rows):
+            for j in range(cols):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
 
+        for i in range(1, rows):
+            for j in range(1, cols):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-        def rec(row, col):
-            if row == 0 or col == 0:
-                return 1
-
-            if dp[row][col] != -1:
-                return dp[row][col]
-
-            up = rec(row-1, col)
-            left = rec(row, col - 1)
-
-            dp[row][col] =  up + left
-            return dp[row][col]
-
-        return rec(rows-1, cols-1)
+        return dp[rows-1][cols-1]
