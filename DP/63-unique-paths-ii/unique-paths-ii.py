fabdebfb39ba112d6729a6f99c969454e7f6b4a5
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-02 22:09:31 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (91.69%) - LeetSync

--- DP/63-unique-paths-ii/unique-paths-ii.py
+++ DP/63-unique-paths-ii/unique-paths-ii.py
@@ -36,29 +36,54 @@ class Solution:
         rows = len(obstacleGrid)
         cols = len(obstacleGrid[0])
 
-        if obstacleGrid[0][0] == 1 or obstacleGrid[rows-1][cols-1] == 1:
-            return 0
+        # if obstacleGrid[0][0] == 1 or obstacleGrid[rows-1][cols-1] == 1:
+        #     return 0
 
-        dp = [[0] * cols for _ in range(rows)]
+        # dp = [[0] * cols for _ in range(rows)]
+
+        # for j in range(cols):
+        #     if obstacleGrid[0][j] == 1:
+        #         dp[0][j] = 0
+        #         break
+        #     dp[0][j] = 1
+
+        # for i in range(rows):
+        #     if obstacleGrid[i][0] == 1:
+        #         dp[i][0] = 0
+        #         break
+        #     dp[i][0] = 1
+
+        # for i in range(1, rows):
+        #     for j in range(1, cols):
+        #         if obstacleGrid[i][j] == 1:
+        #             dp[i][j] = 0
+        #         else:
+        #             dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        # return dp[rows-1][cols-1] 
+
+        #Space Optimized
+
+        prev = [0] * cols
 
         for j in range(cols):
             if obstacleGrid[0][j] == 1:
-                dp[0][j] = 0
                 break
-            dp[0][j] = 1
+            prev[j] = 1
 
-        for i in range(rows):
-            if obstacleGrid[i][0] == 1:
-                dp[i][0] = 0
-                break
-            dp[i][0] = 1
 
         for i in range(1, rows):
-            for j in range(1, cols):
-                if obstacleGrid[i][j] == 1:
-                    dp[i][j] = 0
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            curr = [0] * cols
 
-        return dp[rows-1][cols-1] if dp[rows-1][cols-1] != -1 else 0
- 
+            for j in range(cols):
+                if obstacleGrid[i][j] == 1:
+                    curr[j] = 0
+                else:
+                    up = prev[j]
+                    left = curr[j-1] if j > 0 else 0
+                    curr[j] = up + left
+
+            prev = curr
+
+
+        return prev[cols - 1]
