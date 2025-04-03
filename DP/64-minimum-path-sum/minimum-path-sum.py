23c093d89b848b795737078504c40782255bc00e
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-03 13:14:27 -0400

    Time: 9 ms (94.36%) | Memory: 18.6 MB (97.47%) - LeetSync

--- DP/64-minimum-path-sum/minimum-path-sum.py
+++ DP/64-minimum-path-sum/minimum-path-sum.py
@@ -30,18 +30,36 @@ class Solution:
 
         #Tabulation
 
-        dp = [ [-1 for _ in range(cols)] for _ in range(rows)]
+        # dp = [ [-1 for _ in range(cols)] for _ in range(rows)]
 
-        dp[0][0] = grid[0][0]
+        # dp[0][0] = grid[0][0]
 
-        for i in range(1, cols):
-            dp[0][i] = dp[0][i-1] + grid[0][i]
+        # for i in range(1, cols):
+        #     dp[0][i] = dp[0][i-1] + grid[0][i]
 
-        for j in range(1, rows):
-            dp[j][0] = dp[j-1][0] + grid[j][0]
+        # for j in range(1, rows):
+        #     dp[j][0] = dp[j-1][0] + grid[j][0]
+
+        # for i in range(1, rows):
+        #     for j in range(1, cols):
+        #         dp[i][j] = min(dp[i-1][j] + grid[i][j], dp[i][j-1] + grid[i][j])
+
+        # return dp[rows-1][cols-1]
+
+        #Space Optimize
+
+        dp = [0] * cols
+
+        dp[0] = grid[0][0]
+
+        for j in range(1, cols):
+            dp[j] = dp[j-1] + grid[0][j]
 
         for i in range(1, rows):
-            for j in range(1, cols):
-                dp[i][j] = min(dp[i-1][j] + grid[i][j], dp[i][j-1] + grid[i][j])
+            dp[0] += grid[i][0]
 
-        return dp[rows-1][cols-1]
+            for j in range(1, cols):
+                dp[j] = min(dp[j], dp[j-1]) + grid[i][j]
+
+
+        return dp[-1]
