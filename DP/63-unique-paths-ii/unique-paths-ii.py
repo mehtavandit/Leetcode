07efd9135344f65c8a812837fbeaeb8e6e511638
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-02 22:00:47 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (48.89%) - LeetSync

--- DP/63-unique-paths-ii/unique-paths-ii.py
+++ DP/63-unique-paths-ii/unique-paths-ii.py
@@ -1,30 +1,64 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         
+        # rows = len(obstacleGrid)
+        # cols = len(obstacleGrid[0])
+
+        #Memoization
+
+        # if obstacleGrid[0][0] == 1 or obstacleGrid[rows-1][cols-1] == 1:
+        #     return 0
+
+        # dp = [[-1] * cols for _ in range(rows)]
+
+        # def rec(row, col):
+        #     if row < 0 or col < 0 or obstacleGrid[row][col] == 1:
+        #         return 0
+
+        #     if row==0 and col == 0:
+        #         return 1
+
+        #     if dp[row][col] != -1:
+        #         return dp[row][col]
+
+        #     up = rec(row-1,col)
+        #     left = rec(row, col-1)
+
+        #     dp[row][col] = up + left
+
+        #     return dp[row][col]
+
+
+        # return rec(rows-1, cols-1)
+
+        #tabulation
+
         rows = len(obstacleGrid)
         cols = len(obstacleGrid[0])
 
         if obstacleGrid[0][0] == 1 or obstacleGrid[rows-1][cols-1] == 1:
             return 0
 
-        dp = [[-1] * cols for _ in range(rows)]
+        dp = [[0] * cols for _ in range(rows)]
 
-        def rec(row, col):
-            if row < 0 or col < 0 or obstacleGrid[row][col] == 1:
-                return 0
+        for j in range(cols):
+            if obstacleGrid[0][j] == 1:
+                dp[0][j] = 0
+                break
+            dp[0][j] = 1
 
-            if row==0 and col == 0:
-                return 1
+        for i in range(rows):
+            if obstacleGrid[i][0] == 1:
+                dp[i][0] = 0
+                break
+            dp[i][0] = 1
 
-            if dp[row][col] != -1:
-                return dp[row][col]
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-            up = rec(row-1,col)
-            left = rec(row, col-1)
-
-            dp[row][col] = up + left
-
-            return dp[row][col]
-
-
-        return rec(rows-1, cols-1)
+        return dp[rows-1][cols-1] if dp[rows-1][cols-1] != -1 else 0
+ 
