7a1558b3f6d2284f153b1654af40282ba2865b91
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-03 13:57:34 -0400

    Time: 4 ms (54.52%) | Memory: 19.3 MB (43.44%) - LeetSync

--- DP/120-triangle/triangle.py
+++ DP/120-triangle/triangle.py
@@ -5,19 +5,31 @@ class Solution:
 
         dp = [ [-1 for j in range(n)] for i in range(n)]
         
-        def rec(row, col):
-            if row == n-1:
-                return triangle[n-1][col]
+        #Memoization
+        # def rec(row, col):
+        #     if row == n-1:
+        #         return triangle[n-1][col]
 
-            if dp[row][col] != -1:
-                return dp[row][col]
+        #     if dp[row][col] != -1:
+        #         return dp[row][col]
 
-            down = triangle[row][col] + rec(row+1,col)
-            dig = triangle[row][col] + rec(row+1,col+1)
+        #     down = triangle[row][col] + rec(row+1,col)
+        #     dig = triangle[row][col] + rec(row+1,col+1)
 
-            dp[row][col] = min(down, dig)
-            return dp[row][col]
+        #     dp[row][col] = min(down, dig)
+        #     return dp[row][col]
+        # return rec(0,0)
 
+        #Tabulation
 
+        for j in range(n):
+            dp[n-1][j] = triangle[n-1][j]
 
-        return rec(0,0)
+        for i in range(n-2, -1, -1):
+            for j in range(i, -1, -1):
+                down = triangle[i][j] + dp[i+1][j]
+                dig = triangle[i][j] + dp[i+1][j+1]
+
+                dp[i][j] = min(down, dig)
+
+        return dp[0][0]
