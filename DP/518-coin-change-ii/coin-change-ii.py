a8f5be4bc9aad119234718abc29198fd600d6c6c
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-07 14:24:50 -0400

    Time: 373 ms (39.25%) | Memory: 63.1 MB (23.68%) - LeetSync

--- DP/518-coin-change-ii/coin-change-ii.py
+++ DP/518-coin-change-ii/coin-change-ii.py
@@ -2,28 +2,52 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
 
-        dp = [ [-1 for _ in range(amount+1)] for _ in range(n) ]
+        #Memoization
+        # dp = [ [-1 for _ in range(amount+1)] for _ in range(n) ]
 
-        def rec(index, target):
-            if index == 0:
-                if target % coins[index] == 0:
-                    return 1
-                else:
-                    return 0
+        # def rec(index, target):
+        #     if index == 0:
+        #         if target % coins[index] == 0:
+        #             return 1
+        #         else:
+        #             return 0
 
-            if dp[index][target] != -1:
-                return dp[index][target]
+        #     if dp[index][target] != -1:
+        #         return dp[index][target]
 
-            notTake = 0 + rec(index-1, target)
-            take = 0
+        #     notTake = 0 + rec(index-1, target)
+        #     take = 0
 
-            if coins[index] <= target:
-                take =  rec(index, target - coins[index])
+        #     if coins[index] <= target:
+        #         take =  rec(index, target - coins[index])
 
-            dp[index][target] =  notTake + take
-            return dp[index][target]
+        #     dp[index][target] =  notTake + take
+        #     return dp[index][target]
 
-        return rec(n-1, amount)
+        # return rec(n-1, amount)
+
+        #Tabulation
+
+        dp = [ [0 for _ in range(amount+1)] for _ in range(n) ]
+
+        for target in range(amount + 1):
+            if target % coins[0] == 0:
+                dp[0][target] = 1
+            else:
+                dp[0][target] = 0
+
+
+        for i in range(1, n):
+            for target in range(amount+1):
+                notTake = dp[i-1][target]
+                take = 0
+
+                if coins[i] <= target:
+                    take = dp[i][target-coins[i]]
+
+                dp[i][target] = take + notTake
+
+        return dp[n-1][amount]
 
 
         
