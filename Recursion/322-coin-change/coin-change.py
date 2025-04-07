24dc8c0132f84a1c6b5a19dd4aab281f67a1b660
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-07 11:53:51 -0400

    Time: 1547 ms (6.48%) | Memory: 22.6 MB (21.38%) - LeetSync

--- Recursion/322-coin-change/coin-change.py
+++ Recursion/322-coin-change/coin-change.py
@@ -4,24 +4,49 @@ class Solution:
 
         dp = [ [-1 for _ in range(amount+1)] for _ in range(n)]
 
-        def rec(i, target):
-            if i == 0:
-                if target % coins[i] == 0:
-                    return target // coins[i]
-                else:
-                    return float('inf')
+        #Memoization
+        # def rec(i, target):
+        #     if i == 0:
+        #         if target % coins[i] == 0:
+        #             return target // coins[i]
+        #         else:
+        #             return float('inf')
 
-            if dp[i][target] != -1:
-                return dp[i][target]
+        #     if dp[i][target] != -1:
+        #         return dp[i][target]
 
-            notTake = 0 + rec(i-1, target)
-            take = float('inf')
+        #     notTake = 0 + rec(i-1, target)
+        #     take = float('inf')
 
-            if coins[i] <= target:
-                take = 1 + rec(i, target - coins[i])
+        #     if coins[i] <= target:
+        #         take = 1 + rec(i, target - coins[i])
 
-            dp[i][target] = min(notTake, take)
-            return dp[i][target]
+        #     dp[i][target] = min(notTake, take)
+        #     return dp[i][target]
 
-        res =  rec(n-1, amount)
-        return -1 if res == float('inf') else res
+        # res =  rec(n-1, amount)
+        # return -1 if res == float('inf') else res
+
+        #Tabulation
+
+        dp = [ [0 for _ in range(amount+1)] for _ in range(n)]
+
+        for target in range(0, amount+1):
+            if target % coins[0] == 0:
+                dp[0][target] = target // coins[0]
+            else:
+                dp[0][target] = float('inf')
+
+        for i in range(1, n):
+            for target in range(0, amount + 1):
+                notTake = 0 + dp[i-1][target]
+                take = float('inf')
+
+                if coins[i] <= target:
+                    take = 1 + dp[i][target - coins[i]]
+
+                dp[i][target] = min(notTake, take)
+
+        res = dp[n-1][amount]
+
+        return -1 if res == float('inf') else res 
