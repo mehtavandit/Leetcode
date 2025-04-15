eae953e56dce1bcefb9be8b03fb4d78831412ad6
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-15 17:37:18 -0400

    Time: 303 ms (76.87%) | Memory: 18.1 MB (80.23%) - LeetSync

--- DP/44-wildcard-matching/wildcard-matching.py
+++ DP/44-wildcard-matching/wildcard-matching.py
@@ -36,23 +36,44 @@ class Solution:
 
         #Tabulation
 
-        dp = [ [False for _ in range(l2+1)] for _ in range(l1+1)]
+        # dp = [ [False for _ in range(l2+1)] for _ in range(l1+1)]
 
-        dp[0][0] = True
+        # dp[0][0] = True
 
-        for i1 in range(1, l1+1):
-            if p[i1-1] == "*":
-                dp[i1][0] = dp[i1-1][0]
-            else:
-                break
+        # for i1 in range(1, l1+1):
+        #     if p[i1-1] == "*":
+        #         dp[i1][0] = dp[i1-1][0]
+        #     else:
+        #         break
 
-        for i1 in range(1, l1+1):
-            for i2 in range(1, l2+1):
-                if p[i1-1] == s[i2-1] or p[i1-1] == '?':
-                    dp[i1][i2] = dp[i1-1][i2-1]
-                elif p[i1-1] == "*":
-                    dp[i1][i2] = dp[i1-1][i2] or dp[i1][i2-1]
+        # for i1 in range(1, l1+1):
+        #     for i2 in range(1, l2+1):
+        #         if p[i1-1] == s[i2-1] or p[i1-1] == '?':
+        #             dp[i1][i2] = dp[i1-1][i2-1]
+        #         elif p[i1-1] == "*":
+        #             dp[i1][i2] = dp[i1-1][i2] or dp[i1][i2-1]
+        #         else:
+        #             dp[i1][i2] = False
+
+        # return dp[l1][l2]
+
+        prev = [False] * (l2 + 1)
+        curr = [False] * (l2 + 1)
+
+        prev[0] = True
+
+        for i in range(1, l1 + 1):
+            curr[0] = prev[0] if p[i - 1] == '*' else False
+
+            for j in range(1, l2 + 1):
+                if p[i - 1] == s[j - 1] or p[i - 1] == '?':
+                    curr[j] = prev[j - 1]
+                elif p[i - 1] == '*':
+                    curr[j] = prev[j] or curr[j - 1]
                 else:
-                    dp[i1][i2] = False
+                    curr[j] = False
 
-        return dp[l1][l2]
+            prev, curr = curr, [False] * (l2 + 1)
+
+        return prev[l2]
+
