0a7957d84c85013c8a603918ef5d95d01da2644c
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-23 14:43:05 -0400

    Time: 15 ms (6.02%) | Memory: 21.5 MB (5.54%) - LeetSync

--- DP/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ DP/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -2,24 +2,45 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
 
-        dp = [ [-1 for _ in range(2)] for _ in range(n)]
+        #Memoization
 
-        def rec(ind, buy):
-            if ind == n:
-                return 0
+        # dp = [ [-1 for _ in range(2)] for _ in range(n)]
 
-            if dp[ind][buy] != -1:
-                return dp[ind][buy]
-            if buy == 1:
-                take = -prices[ind] + rec(ind+1, 0)
-                notTake = 0 + rec(ind+1, 1)
-                profit = max(take, notTake)
-            else:
-                take = prices[ind] + rec(ind+1, 1)
-                notTake = 0 + rec(ind+1, 0)
-                profit = max(take, notTake)
+        # def rec(ind, buy):
+        #     if ind == n:
+        #         return 0
 
-            dp[ind][buy] = profit
-            return profit
+        #     if dp[ind][buy] != -1:
+        #         return dp[ind][buy]
+        #     if buy == 1:
+        #         take = -prices[ind] + rec(ind+1, 0)
+        #         notTake = 0 + rec(ind+1, 1)
+        #         profit = max(take, notTake)
+        #     else:
+        #         take = prices[ind] + rec(ind+1, 1)
+        #         notTake = 0 + rec(ind+1, 0)
+        #         profit = max(take, notTake)
 
-        return rec(0,1)
+        #     dp[ind][buy] = profit
+        #     return profit
+
+        # return rec(0,1)
+
+        #Tabulation
+
+        dp = [ [-1 for _ in range(2)] for _ in range(n+1)]
+
+        dp[n][0] = dp[n][1] = 0
+
+        for ind in range(n-1, -1, -1):
+            for buy in range(2):
+                profit = 0
+
+                if buy == 1:
+                    profit = max(0 + dp[ind+1][1], -prices[ind] + dp[ind+1][0])
+                else:
+                    profit = max(0 + dp[ind+1][0], prices[ind] + dp[ind+1][1])
+
+                dp[ind][buy] = profit
+
+        return dp[0][1]
