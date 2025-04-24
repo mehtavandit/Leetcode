a7efa94a38863f2679952589f1cbe0d9ab0157b0
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-24 12:13:58 -0400

    Time: 3 ms (77.97%) | Memory: 18.5 MB (60.04%) - LeetSync

--- DP/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ DP/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,21 +3,34 @@ class Solution:
         
         n = len(prices)
 
-        dp = [ [-1 for _ in range(2)] for _ in range(n)]
+        # Memo
+        # dp = [ [-1 for _ in range(2)] for _ in range(n)]
 
-        def rec(ind, buy):
-            if ind >= n:
-                return 0
+        # def rec(ind, buy):
+        #     if ind >= n:
+        #         return 0
 
-            if dp[ind][buy] != -1:
-                return dp[ind][buy]
+        #     if dp[ind][buy] != -1:
+        #         return dp[ind][buy]
 
-            if buy == 1:
-                profit = max(-prices[ind] + rec(ind+1, 0), 0 + rec(ind+1, 1))
-            else:
-                profit = max(prices[ind] + rec(ind+2, 1), 0 + rec(ind+1, 0))
+        #     if buy == 1:
+        #         profit = max(-prices[ind] + rec(ind+1, 0), 0 + rec(ind+1, 1))
+        #     else:
+        #         profit = max(prices[ind] + rec(ind+2, 1), 0 + rec(ind+1, 0))
 
-            dp[ind][buy] = profit
-            return dp[ind][buy]
+        #     dp[ind][buy] = profit
+        #     return dp[ind][buy]
 
-        return rec(0,1)
+        # return rec(0,1)
+
+        dp = [ [0 for _ in range(2)] for _ in range(n+2)]
+
+        for ind in range(n-1, -1, -1):
+            for buy in range(2):
+                if buy == 1:
+                    dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+                else:
+                    dp[ind][buy] = max(prices[ind] + dp[ind+2][1], 0 + dp[ind+1][0])
+
+        
+        return dp[0][1]
