3b49f05a9a00fc274a4a7cfc558437f8327bf3f1
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-24 12:19:48 -0400

    Time: 3 ms (77.97%) | Memory: 18 MB (92.42%) - LeetSync

--- DP/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ DP/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -23,14 +23,35 @@ class Solution:
 
         # return rec(0,1)
 
-        dp = [ [0 for _ in range(2)] for _ in range(n+2)]
+        #Tabulation
+
+        # dp = [ [0 for _ in range(2)] for _ in range(n+2)]
+
+        # for ind in range(n-1, -1, -1):
+        #     for buy in range(2):
+        #         if buy == 1:
+        #             dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+        #         else:
+        #             dp[ind][buy] = max(prices[ind] + dp[ind+2][1], 0 + dp[ind+1][0])
+
+        
+        # return dp[0][1]
+
+        #Space Optimized
+
+        curr = [0,0]
+        front1 = [0,0]
+        front2 = [0,0]
 
         for ind in range(n-1, -1, -1):
             for buy in range(2):
                 if buy == 1:
-                    dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+                    curr[buy] = max(-prices[ind] + front1[0], 0 + front1[1])
                 else:
-                    dp[ind][buy] = max(prices[ind] + dp[ind+2][1], 0 + dp[ind+1][0])
+                    curr[buy] = max(prices[ind] + front2[1], 0 + front1[0])
 
-        
-        return dp[0][1]
+            front2 = front1.copy()
+            front1 = curr.copy()
+
+
+        return curr[1]
