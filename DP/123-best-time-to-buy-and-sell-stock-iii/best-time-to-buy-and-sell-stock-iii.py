486b14014e33be1664b9c520897f3a3096d986ef
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-23 23:25:45 -0400

    Time: 2504 ms (5.03%) | Memory: 94.2 MB (11.88%) - LeetSync

---
+++ DP/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        dp = [ [ [-1 for _ in range(3)] for _ in range(2) ] for _ in range(n)]
+
+        print(dp)
+
+        def rec(ind, buy, cap):
+            if cap == 0:
+                return 0
+            
+            if ind == n:
+                return 0
+
+            if dp[ind][buy][cap] != -1:
+                return dp[ind][buy][cap]
+
+            if buy == 1:
+                profit = max(-prices[ind] + rec(ind+1, 0, cap), 0 + rec(ind+1,1, cap))
+            else:
+                profit = max(prices[ind] + rec(ind+1, 1, cap-1), 0 + rec(ind+1, 0, cap))
+
+            dp[ind][buy][cap] =  profit
+            return dp[ind][buy][cap]
+
+        return rec(0, 1, 2)
