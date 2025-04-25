846d60e818cd372d989aaf819ea3a5e6cfd06c93
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-25 11:13:44 -0400

    Time: 368 ms (23.30%) | Memory: 30 MB (22.21%) - LeetSync

--- DP/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ DP/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,22 +2,36 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
 
-        dp = [ [-1 for _ in range(2)] for _ in range(n)]
+        #Memo
+        # dp = [ [-1 for _ in range(2)] for _ in range(n)]
 
-        def rec(ind, buy):
-            if ind == n:
-                return 0
+        # def rec(ind, buy):
+        #     if ind == n:
+        #         return 0
             
-            if dp[ind][buy] != -1:
-                return dp[ind][buy]
+        #     if dp[ind][buy] != -1:
+        #         return dp[ind][buy]
 
-            if buy == 1:
-                dp[ind][buy] = max(-prices[ind] + rec(ind+1, 0), 0 + rec(ind+1, 1))
-            else:
-                dp[ind][buy] = max(prices[ind] + rec(ind+1, 1) - fee, 0 + rec(ind+1, 0))
+        #     if buy == 1:
+        #         dp[ind][buy] = max(-prices[ind] + rec(ind+1, 0), 0 + rec(ind+1, 1))
+        #     else:
+        #         dp[ind][buy] = max(prices[ind] + rec(ind+1, 1) - fee, 0 + rec(ind+1, 0))
 
-            return dp[ind][buy]
+        #     return dp[ind][buy]
 
-        return rec(0,1)
+        # return rec(0,1)
+
+        #Tabulation
+
+        dp = [ [0 for _ in range(2)] for _ in range(n+1)]
+
+        for ind in range(n-1, -1, -1):
+            for buy in range(2):
+                if buy == 1:
+                    dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+                else:
+                    dp[ind][buy] = max(prices[ind] + dp[ind+1][1] - fee, 0+dp[ind+1][0])
+
+        return dp[0][1]
 
             
