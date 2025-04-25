5bd4026e2624e4263adc7679b27ec15f65374aa0
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-25 11:18:59 -0400

    Time: 203 ms (37.56%) | Memory: 23.5 MB (55.53%) - LeetSync

--- DP/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ DP/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -23,15 +23,31 @@ class Solution:
 
         #Tabulation
 
-        dp = [ [0 for _ in range(2)] for _ in range(n+1)]
+        # dp = [ [0 for _ in range(2)] for _ in range(n+1)]
+
+        # for ind in range(n-1, -1, -1):
+        #     for buy in range(2):
+        #         if buy == 1:
+        #             dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+        #         else:
+        #             dp[ind][buy] = max(prices[ind] + dp[ind+1][1] - fee, 0+dp[ind+1][0])
+
+        # return dp[0][1]
+
+        #Space optimized
+
+        ahead = [0,0]
+        curr = [0,0]
 
         for ind in range(n-1, -1, -1):
             for buy in range(2):
                 if buy == 1:
-                    dp[ind][buy] = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+                    curr[buy] = max(-prices[ind] + ahead[0], 0 + ahead[1])
                 else:
-                    dp[ind][buy] = max(prices[ind] + dp[ind+1][1] - fee, 0+dp[ind+1][0])
+                    curr[buy] = max(prices[ind] + ahead[1] - fee, 0 + ahead[0])
 
-        return dp[0][1]
+            ahead = curr.copy()
+
+        return curr[1]
 
             
