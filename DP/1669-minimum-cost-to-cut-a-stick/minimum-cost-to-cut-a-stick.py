406546363722e859866fe0a77d88cb172a329e7e
Vandit Mehta <65803900+mehtavandit@users.noreply.github.com>
2025-04-28 11:05:17 -0400

    Time: 479 ms (76.25%) | Memory: 18.1 MB (93.18%) - LeetSync

--- DP/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ DP/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -2,25 +2,44 @@ class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
         cuts = [0] + cuts + [n]
         cuts.sort()
-        c = len(cuts)
+        c = len(cuts) - 2
 
-        dp = [ [-1 for _ in range(c+1)] for _ in range(c+1) ]
+        # dp = [ [-1 for _ in range(c+1)] for _ in range(c+1) ]
 
-        def rec(i, j):
-            if i>j:
-                return 0
+        # def rec(i, j):
+        #     if i>j:
+        #         return 0
 
-            if dp[i][j] != -1:
-                return dp[i][j]
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
 
-            mini = float('inf')
+        #     mini = float('inf')
 
-            for ind in range(i, j+1):
-                ans = cuts[j+1] - cuts[i-1] + rec(i, ind-1) + rec(ind+1, j)
-                mini = min(mini, ans)
+        #     for ind in range(i, j+1):
+        #         ans = cuts[j+1] - cuts[i-1] + rec(i, ind-1) + rec(ind+1, j)
+        #         mini = min(mini, ans)
 
-            dp[i][j] =  mini
-            return dp[i][j]
+        #     dp[i][j] =  mini
+        #     return dp[i][j]
 
 
-        return rec(1, len(cuts)-2)
+        # return rec(1, len(cuts)-2)
+
+        #Tabulation
+
+        dp = [ [0 for _ in range(c+2)] for _ in range(c+2)]
+
+        for i in range(c, 0, -1):
+            for j in range(1, c+1):
+                if i>j:
+                    continue
+                
+                mini = float('inf')
+
+                for ind in range(i, j+1):
+                    ans = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j]
+                    mini = min(mini, ans)
+
+                dp[i][j] = mini
+
+        return dp[1][c]
